4cbff073177f2218fa9d16b0010b0e64ddb54b33
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2026-04-05 13:54:12 -0400

    make function out of lazy loading code

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -14,31 +14,19 @@ $(document).ready(function () {
 
             $('#gallery').append(newLink);
 
-            // Lazy loading fallback
-            if ('loading' in HTMLImageElement.prototype) {
-
-                var images = document.querySelectorAll('img[loading="lazy"]');
-
-                for (var i = 0; i < images.length; i++) {
-                    images[i].src = images[i].dataset.src;
-                    images[i].onload = function(e) {
-                        e.target.classList.add('loaded');
-                    }
-                }
-
-            } else {
-                // Dynamically import the LazySizes library
-                var script = document.createElement('script');
-                script.async = true;
-                script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js';
-
-                document.body.appendChild(script);
-            }
+            lazyLoading();
         }
     });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+    lazyLoading();
+
+    // object-fit and object-position polyfill
+    objectFitImages();
+});
+
+function lazyLoading() {
     // Lazy loading fallback
     if ('loading' in HTMLImageElement.prototype) {
 
@@ -59,7 +47,4 @@ document.addEventListener('DOMContentLoaded', function() {
 
         document.body.appendChild(script);
     }
-
-    // object-fit and object-position polyfill
-    objectFitImages();
-});
+}
