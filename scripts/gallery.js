3481c6a097081126ba9a290cdf9b56fdfcbeccd4
Isbo2000 <94263966+Isbo2000@users.noreply.github.com>
2026-04-05 14:53:31 -0400

    redo gallery.json with groups

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
-    url = 'https://isbo.cc/assets/gallery.json';
+    url = 'https://storage.filebin.net/filebin/2ccf3db92d489a2fd213cce6d020d5aff226b395099d902a0bbb571c90b1ea86?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=GK352fd2505074fc9dde7fd2cb%2F20260405%2Fhel1-dc4%2Fs3%2Faws4_request&X-Amz-Date=20260405T184626Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&response-cache-control=max-age%3D900&response-content-disposition=inline%3B%20filename%3D%22gallery.json%22&response-content-type=application%2Fjson&x-id=GetObject&X-Amz-Signature=6203a70767f9a353cae8a41bb88be8432399e8668c1fb05bac769a999bdc3ed2';
     fetch(url).then(response => {return response.json();}).then(function (data) {
-        for (let link in data) {
-            let newLink = "";
+        for (var group in data) {
+            var newGroup = "";
 
-            newLink += "<li><a href='";
-            newLink += data[link].url;
-            newLink += "' data-lightbox='";
-            newLink += data[link].group;
-            newLink += "'><img class='lazyload' data-src='";
-            newLink += data[link].url;
-            newLink += "' loading='lazy'/></a></li>";
+            newGroup += "<li class='photogallery'><ul>";
+            newGroup += "<h1>test</h1>"
 
-            $('#gallery').append(newLink);
+            for (let link in data[group].images) {
+                let newLink = "";
+
+                newLink += "<li><a href='";
+                newLink += data[group].images[link];
+                newLink += "' data-lightbox='";
+                newLink += data[group].group;
+                newLink += "'><img class='lazyload' data-src='";
+                newLink += data[group].images[link];
+                newLink += "' loading='lazy'/></a></li>";
+
+                newGroup += newLink;
+            }
+            
+            newGroup += "</ul></li>";
+
+            $('#gallery').append(newGroup);
 
             lazyLoading();
         }
